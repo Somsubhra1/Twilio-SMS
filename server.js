8ebd8bcd744a9160ef6f4ffe4d9b5db06937fbde
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-01 14:33:52 +0530

    fixed var name

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv/config");
 var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
 var authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
-var twilio = require("twilio")(accountSid, authToken);
+var client = require("twilio")(accountSid, authToken);
 const app = express();
 
 app.use(express.json());
